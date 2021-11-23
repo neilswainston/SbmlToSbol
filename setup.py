5ebdf6b3cdde93b3ee6c30c54831af27e7af7b5d
Joan Hérisson <joan.herisson@univ-evry.fr>
2021-11-23 23:53:44 +0100

    build(setup.py): specify python_requires

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires               = '>=3.7',
+    python_requires               = '>=3.7, <3.8',
 )
 
